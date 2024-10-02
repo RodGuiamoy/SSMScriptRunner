e2ad9a1c784050273aded50d922b9f9234a332f9
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-10-02 17:37:59 -0400

    case insensitive instance id identifiction (inefficient)

--- 00_get_instance_id.py
+++ 00_get_instance_id.py
@@ -2,27 +2,23 @@ import boto3
 import sys
 
 def get_instance_id(instance_name, region):
-    # Create a session using the specified region
+    # Create a session using your AWS credentials
     session = boto3.Session(region_name=region)
-    
-    # Create an EC2 client
     ec2_client = session.client('ec2')
-    
-    # Describe instances with filters
-    response = ec2_client.describe_instances(
-        Filters=[
-            {
-                'Name': 'tag:Name',
-                'Values': [instance_name]
-            }
-        ]
-    )
-    
-    # Iterate through the reservations and instances to find the instance ID
+
+    # Describe all instances
+    response = ec2_client.describe_instances()
+
+    # Loop through all instances to find the one with the specified name
     for reservation in response['Reservations']:
         for instance in reservation['Instances']:
-            return instance['InstanceId']
+            # Check if the 'Name' tag matches the instance_name (case insensitive)
+            tags = instance.get('Tags', [])
+            for tag in tags:
+                if tag['Key'] == 'Name' and tag['Value'].lower() == instance_name.lower():
+                    return instance['InstanceId']
     
+    # If no instance is found, return None
     return None
 
 instance_name = sys.argv[1]
